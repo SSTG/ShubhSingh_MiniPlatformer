ec25a1fae356810632fd53fa5c04c1aee5caa684
SSTG <67901394+SSTG@users.noreply.github.com>
2024-02-29 09:07:48 +0530

    Movement and Jump Jitter Fixed

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -7,11 +7,20 @@ public class PlayerMovement : MonoBehaviour
     // Start is called before the first frame update
     Rigidbody rb;
     [SerializeField]float jumpForce;
+    [SerializeField]LayerMask surfaceLayers;
+    [SerializeField]float moveSpeedMultiplier;
     [SerializeField]float surfaceCheckRadius;
     [SerializeField]float dashForce;
+    bool isGrounded;
+
     void Start()
     {
         rb=GetComponent<Rigidbody>();
+    }
+    void Update()
+    {
+        isGrounded = Physics.CheckSphere(transform.position,surfaceCheckRadius,surfaceLayers);
+      
     }
     void OnEnable()
     {
@@ -27,12 +36,12 @@ public class PlayerMovement : MonoBehaviour
     }
     void MovePlayer(float movement)
     {
-        rb.velocity=new Vector3(movement,0,0);
+        rb.velocity=new Vector3(movement*moveSpeedMultiplier,rb.velocity.y,0);
     }
     void JumpPlayer()
     {
-        if(Physics.CheckSphere(transform.position,surfaceCheckRadius))
-        rb.AddForce(new Vector3(0,jumpForce,0),ForceMode.Force);
+        if(isGrounded)
+        rb.velocity+=new Vector3(0,jumpForce,0);
     }
     void DashPlayer()
     {
